762d05870af526cb2abe9abdb8d7fccbab69879e
Stromwars <110508620+Stromwars@users.noreply.github.com>
2022-08-07 18:51:14 -0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,13 @@
+#Belajar output
+
+print("hello")
+print("nama saya DAFFA ENDRAMAWAN")
+print("saya berasal dari indonesia")
+print("saya pelajar smk")
+print("hobi saya bermain sepak bola")
+print("cita-cita bermain di timnas indonesia")
+print("membawa pulang piala dunia untuk indonesia adalah impian saya")
+print("dilain waktu saya juga senang bermain game")
+print("cukup sampai sini perkenalan dari saya")
+print("terimakasii")
+print("TUTOR LANCE NGAB???")
---
+++ python2.py
@@ -0,0 +1,26 @@
+# Bejalar input
+
+nama = "dapa"
+umur = 15
+alamat = "Cimanggis, Kota Depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("Nama saya adalah " + nama)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
