305eba2e6b89f63a93bda4af461e61267c125bae
Stromwars <110508620+Stromwars@users.noreply.github.com>
2022-08-11 20:41:10 -0700

    Add files via upload

---
+++ UPRAK_DAFFA ENDRAMAWAN_X TKJ1.txt.py
@@ -0,0 +1,18 @@
+nama =input("Masukan Nama Depan: ")
+nama = + input("masukan nama belakang: ")
+
+print = input("Nama Anda Adalah" + nama_depan + nama)
+
+
+
+
+print("Komputer ini milik dapa")
+
+nama = input("Masukan Nama Anda: ")
+kelas = input("Masukan Kelas Anda: ")
+print("Nama Anda Adalah", nama, "dan Anda kelas",kelas)
+
+
+celcius = int(input("Masukan Suhu Celcius: "))
+fah = (9/5 * celcius) + 80 
+print(celcius, "Celcius sama dengan", fah, "Fahreinhet")
